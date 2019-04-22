e52755deb79d99b1146365753c4fcb4dbbdc81fe
KKFantasy <kangke1221@gmail.com>
2019-04-22 12:59:00 +0800

    demo, 截快照

--- ARDemo/ViewController.swift
+++ ARDemo/ViewController.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2019 kk. All rights reserved.
 //
 
-import UIKit
-import SceneKit
 import ARKit
+import SceneKit
+import UIKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -28,6 +28,63 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        // 添加点击事件
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapScreen))
+        view.addGestureRecognizer(tap)
+    }
+    
+    // 点击屏幕
+    @objc func tapScreen() {
+        // 截图并创建平面
+        let imagePlane = SCNPlane(width: sceneView.bounds.width / 6000, height: sceneView.bounds.height / 6000)
+        imagePlane.firstMaterial?.diffuse.contents = sceneView.snapshot()
+        imagePlane.firstMaterial?.lightingModel = .constant
+        
+        // 创建plane node并添加到场景
+        let planeNode = SCNNode(geometry: imagePlane)
+        sceneView.scene.rootNode.addChildNode(planeNode)
+        
+        // 改变其在空间的位置为摄像头的位置
+        let currentFrame = sceneView.session.currentFrame
+        if let transform = currentFrame?.camera.transform {
+            var translation = matrix_identity_float4x4
+            updateTranslationMatrix(&translation)
+            planeNode.simdTransform = matrix_multiply(transform, translation)
+            
+        }
+    }
+    
+    func updateTranslationMatrix(_ translation: inout simd_float4x4) {
+        switch UIDevice.current.orientation{
+        case .portrait, .portraitUpsideDown, .unknown, .faceDown, .faceUp:
+            print("portrait ")
+            translation.columns.0.x = -cos(.pi/2)
+            translation.columns.0.y = sin(.pi/2)
+            translation.columns.1.x = -sin(.pi/2)
+            translation.columns.1.y = -cos(.pi/2)
+        case .landscapeLeft :
+            print("landscape left")
+            translation.columns.0.x = 1
+            translation.columns.0.y = 0
+            translation.columns.1.x = 0
+            translation.columns.1.y = 1
+        case .landscapeRight :
+            print("landscape right")
+            translation.columns.0.x = cos(.pi)
+            translation.columns.0.y = -sin(.pi)
+            translation.columns.1.x = sin(.pi)
+            translation.columns.1.y = cos(.pi)
+        default:
+            break
+        }
+        translation.columns.3.z = -0.15 //60cm in front of the camera
+    }
+    
+    @IBAction func Clear(_ sender: UIButton) {
+        sceneView.scene.rootNode.enumerateChildNodes({ node, _ in
+            node.removeFromParentNode()
+        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +92,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,30 +103,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+         let node = SCNNode()
      
-        return node
-    }
-*/
+         return node
+     }
+     */
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
